9ae6570b1df945412044c6eadae03310a8d7f3d1
Richard Krijnse Locker <richard.krijnselocker@kpn.com>
2022-11-15 14:41:30 +0100

    mvp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,92 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import {useState} from "react";
+
+export default function App() {
+  const [sleptOn, setSleptOn] = useState('Selecteer...');
+  const [shouldSleepOn, setShouldSleepOn] = useState('');
+  const [time, setTime] = useState('');
+
+  const [isLeftDisabled, setIsLeftDisabled] = useState(false);
+  const [isRightDisabled, setIsRightDisabled] = useState(false);
+
+  const onLeftPress = () => {
+    setIsRightDisabled(false);
+    setIsLeftDisabled(true);
+    setShouldSleepOn('rechts');
+    setSleptOn('links')
+    setTimeStamp();
+  }
+
+  const onRightPress = () => {
+    setIsRightDisabled(true);
+    setIsLeftDisabled(false);
+    setShouldSleepOn('links');
+    setSleptOn('rechts')
+    setTimeStamp();
+  }
+
+  const setTimeStamp = () => {
+    const time = new Intl.DateTimeFormat("nl", {
+      timeStyle: "short",
+    });
+    setTime(time.format(Date.now()))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <div>
+          <p>Ze sliep {sleptOn}</p>
+          <p>Ze moet {shouldSleepOn} liggen</p>
+          <p>Aangepast op: {time}</p>
+        </div>
+
+        <div>
+          <button onClick={onLeftPress}>
+            <p>Links</p>
+          </button>
+          <button onClick={onRightPress}>
+            <p>Rechts</p>
+          </button>
+        </div>
+      </div>
   );
 }
 
-export default App;
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#2D1E2F',
+//     alignItems: 'center',
+//     justifyContent: 'space-around',
+//
+//   },
+//   text: {
+//     padding: 10,
+//     color: '#F7B32B',
+//     fontSize: '25px'
+//   },
+//   buttonContainer: {
+//     display: "flex",
+//     flexDirection: "row"
+//   },
+//   button: {
+//     alignItems: 'center',
+//     margin: 10,
+//     width: 150,
+//     justifyContent: 'center',
+//     paddingVertical: 12,
+//     paddingHorizontal: 32,
+//     borderRadius: 4,
+//     backgroundColor: '#547AA5',
+//   },
+//   buttonText: {
+//     fontSize: 16,
+//     lineHeight: 21,
+//     fontWeight: 'bold',
+//     letterSpacing: 0.25,
+//     color: 'white',
+//   },
+// });
+
